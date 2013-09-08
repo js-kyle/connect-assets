233f0e8827482caab317636fc27b7e420a72533c
Andrew Dunkman <adunkman@getharvest.com>
2013-09-08 11:50:56 -0400

    Reorganize tests for shorter files

--- test/serveAsset.js
+++ test/serveAsset.js
@@ -4,21 +4,7 @@ var http = require("http");
 var fs = require("fs");
 var createServer = require("./testHelpers/createServer");
 
-describe("serveAsset", function () {
-  it("allows the served path to be changed", function (done) {
-    createServer({ servePath: "arsets" }, function () {
-      var path = this.assetPath("blank.js");
-      var url = this.host + path;
-
-      expect(path).to.contain("/arsets/");
-
-      http.get(url, function (res) {
-        expect(res.statusCode).to.equal(200);
-        done();
-      });
-    });
-  });
-
+describe("serveAsset file changes", function () {
   it("serves new files as they exist on disk in development", function (done) {
     createServer({}, function () {
       var file = "test/assets/css/new-file.css";
@@ -91,42 +77,6 @@ describe("serveAsset", function () {
           process.env.NODE_ENV = env;
           done();
         });
-      });
-    });
-  });
-
-  it("does not serve assets for URLs outside of serve path", function (done) {
-    createServer({}, function () {
-      var path = this.assetPath("blank.js").replace("/assets", "");
-      var url = this.host + path;
-
-      http.get(url, function (res) {
-        expect(res.statusCode).to.equal(404);
-        done();
-      });
-    });
-  });
-
-  it("does not serve asset if fingerprint doesn't match", function (done) {
-    createServer({}, function () {
-      var path = this.assetPath("blank.js").replace("af7c72e86aadcfde95bb29d286c27034", "436828974cd5282217fcbd406d41e9ca");
-      var url = this.host + path;
-
-      http.get(url, function (res) {
-        expect(res.statusCode).to.equal(404);
-        done();
-      });
-    });
-  });
-
-  it("does not serve asset if fingerprint isn't supplied", function (done) {
-    createServer({}, function () {
-      var path = this.assetPath("blank.js").replace("-af7c72e86aadcfde95bb29d286c27034", "");
-      var url = this.host + path;
-
-      http.get(url, function (res) {
-        expect(res.statusCode).to.equal(404);
-        done();
       });
     });
   });
---
+++ test/serveAsset.paths.js
@@ -0,0 +1,57 @@
+var expect = require("expect.js");
+var mocha = require("mocha");
+var http = require("http");
+var fs = require("fs");
+var createServer = require("./testHelpers/createServer");
+
+describe("serveAsset paths", function () {
+  it("allows the served path to be changed", function (done) {
+    createServer({ servePath: "arsets" }, function () {
+      var path = this.assetPath("blank.js");
+      var url = this.host + path;
+
+      expect(path).to.contain("/arsets/");
+
+      http.get(url, function (res) {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+
+  it("does not serve assets for URLs outside of serve path", function (done) {
+    createServer({}, function () {
+      var path = this.assetPath("blank.js").replace("/assets", "");
+      var url = this.host + path;
+
+      http.get(url, function (res) {
+        expect(res.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+
+  it("does not serve asset if fingerprint doesn't match", function (done) {
+    createServer({}, function () {
+      var path = this.assetPath("blank.js").replace("af7c72e86aadcfde95bb29d286c27034", "436828974cd5282217fcbd406d41e9ca");
+      var url = this.host + path;
+
+      http.get(url, function (res) {
+        expect(res.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+
+  it("does not serve asset if fingerprint isn't supplied", function (done) {
+    createServer({}, function () {
+      var path = this.assetPath("blank.js").replace("-af7c72e86aadcfde95bb29d286c27034", "");
+      var url = this.host + path;
+
+      http.get(url, function (res) {
+        expect(res.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+});
