221c4ad76d4defd545f05cfa0b57dd50cec52179
Andrew Dunkman <adunkman@getharvest.com>
2013-09-08 21:39:13 -0400

    Instead of adding timeout to a test, fake mtimes.
    The setTimeout call was unpredictable (caused intermittent failures) and it
    was generally gross. Instead, let's fake the file's previous modification
    time back in the past to achieve the same results.

--- test/serveAsset.changes.js
+++ test/serveAsset.changes.js
@@ -15,40 +15,38 @@ describe("serveAsset file changes", function () {
 
       http.get(url, function (res) {
         expect(res.statusCode).to.equal(200);
-        fs.unlinkSync(file);
-        done();
+        fs.unlink(file, done);
       });
     });
   });
 
   it("serves file changes as they are made in development", function (done) {
     var file = "test/assets/css/file-changes-dev.css";
-    var instance = this;
     fs.writeFileSync(file, "");
+
+    // Pretend like this file was last touched a minute ago.
+    var stats = fs.statSync(file);
+    var newmtime = new Date(stats.mtime.getTime() - 60 * 1000);
+    fs.utimesSync(file, stats.atime, newmtime);
 
     createServer.call(this, {}, function () {
       var content = "body { color: #fff; }";
       fs.writeFileSync(file, content);
 
-      // We need to let mincer detect the filesystem change — and therefore we
-      // can't use process.nextTick here as that will jump ahead of IO calls.
-      setTimeout(function () {
-        var path = instance.assetPath("file-changes-dev.css");
-        var url = instance.host + path;
-        var body = "";
+      var path = this.assetPath("file-changes-dev.css");
+      var url = this.host + path;
+      var body = "";
 
-        http.get(url, function (res) {
-          expect(res.statusCode).to.equal(200);
+      http.get(url, function (res) {
+        expect(res.statusCode).to.equal(200);
 
-          res.setEncoding("utf8");
-          res.on("data", function (chunk) { body += chunk });
-          res.on("end", function () {
-            expect(body).to.equal(content + "\n");
-            fs.unlinkSync(file);
-            done();
-          });
+        res.setEncoding("utf8");
+        res.on("data", function (chunk) { body += chunk });
+        res.on("end", function () {
+          expect(body).to.equal(content + "\n");
+          fs.unlink(file, done);
         });
-      }, 0);
+      });
     });
   });
 
@@ -73,9 +71,8 @@ describe("serveAsset file changes", function () {
         res.on("data", function (chunk) { body += chunk });
         res.on("end", function () {
           expect(body).to.equal(content + "\n");
-          fs.unlinkSync(file);
           process.env.NODE_ENV = env;
-          done();
+          fs.unlink(file, done);
         });
       });
     });
