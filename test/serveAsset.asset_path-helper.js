75aceeba008dbbb6c3db1ea24e9ddbfcd04e95ea
Andrew Dunkman <adunkman@getharvest.com>
2014-06-19 13:07:47 -1000

    Add newline at end of test file.

--- test/serveAsset.asset_path-helper.js
+++ test/serveAsset.asset_path-helper.js
@@ -13,14 +13,14 @@ describe("serveAsset asset_path environment helper", function () {
 
     createServer.call(this, { buildDir: dir, compile: true }, function () {
       var path = this.assetPath("asset-path-helper.css");
-      var filename = dir + "/asset-path-helper-7e2feaf5cce60b2ef010192da0f7da2b.css";
+      var filename = dir + "/asset-path-helper-d47652c4dfdf3d6bf07b56c506a7aa12.css";
       var url = this.host + path;
 
       http.get(url, function (res) {
         expect(res.statusCode).to.equal(200);
         expect(fs.statSync(dir).isDirectory()).to.equal(true);
         expect(fs.statSync(filename).isFile()).to.equal(true);
-        expect(fs.readFileSync(filename, "utf8")).to.equal("@import \"/assets/asset-20069ab163c070349198aa05124dcaa8.css\";\n");
+        expect(fs.readFileSync(filename, "utf8")).to.equal("@import \"/assets/asset-20069ab163c070349198aa05124dcaa8.css\";\n\n");
 
         process.env.NODE_ENV = env;
         rmrf(dir, done);
