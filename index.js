2fc02406ea171fcbb37980f444da37c091da2cd9
Andrew Dunkman <adunkman@gmail.com>
2014-07-27 15:22:07 -0400

    Remove options.cdn option.
    No need to add this option, we can just alter the behavior slightly and we’re off and running.
    
    When specifying a full path to `options.servePath`, the pathname from that URL will now be used to serve assets.

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var connectAssets = module.exports = function (options) {
   var compilationComplete = false;
   var compilationError;
   var waiting = [];
+  var localServePath = url.parse(options.servePath).pathname.replace(/^\//, "");
 
   options.helperContext.css = assets.helper(tagWriters.css, "css");
   options.helperContext.js = assets.helper(tagWriters.js, "js");
@@ -25,17 +26,11 @@ var connectAssets = module.exports = function (options) {
 
   var middleware = function (req, res, next) {
     var path = url.parse(req.url).pathname.replace(/^\//, "");
-    var servePath = options.servePath;
-    // Need to get only the pathname component of servePath if we are NOT serving from a CDN:
-    if (options.cdn == false) {
-      servePath = url.parse(options.servePath).pathname
-        .replace(/^\//, "");
-    }
 
-    if (path.toLowerCase().indexOf(servePath.toLowerCase()) === 0) {
+    if (path.toLowerCase().indexOf(localServePath.toLowerCase()) === 0) {
       var serve = function (req, res, next) {
         if (compilationError) { next(compilationError); }
-        else { assets.serveAsset(req, res, next, servePath); }
+        else { assets.serveAsset(req, res, next, localServePath); }
       };
 
       if (compilationComplete) { serve(req, res, next); }
@@ -63,7 +58,6 @@ var parseOptions = module.exports._parseOptions = function (options) {
   options.buildDir = options.buildDir != null ? options.buildDir : isDevelopment ? false : "builtAssets";
   options.compile = options.compile != null ? options.compile : true;
   options.compress = options.compress != null ? options.compress : isProduction;
-  options.cdn = options.cdn != null ? options.cdn : false;
 
   if (options.buildDir.replace)
     options.buildDir = options.buildDir.replace(/^\//, "").replace(/\/$/, "");
