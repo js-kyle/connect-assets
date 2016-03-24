9b71fadeee8ae1cddac169f72f7e17a12cd6bbed
Airat Shigapov <contact@airatshigapov.com>
2016-03-24 12:27:33 +0300

    Upgrade mincer to 1.4.1, add postcss, fix assets digests

--- test/serveAsset.asset_path-helper.js
+++ test/serveAsset.asset_path-helper.js
@@ -13,14 +13,14 @@ describe("serveAsset asset_path environment helper", function () {
 
     createServer.call(this, { buildDir: dir, compile: true, fingerprinting: true }, function () {
       var path = this.assetPath("asset-path-helper.css");
-      var filename = dir + "/asset-path-helper-791d4908176a09d310ccc381f9306283.css";
+      var filename = dir + "/asset-path-helper-ecc5d891145d56a0274eb4f34670671e.css";
       var url = this.host + path;
 
       http.get(url, function (res) {
         expect(res.statusCode).to.equal(200);
         expect(fs.statSync(dir).isDirectory()).to.equal(true);
         expect(fs.statSync(filename).isFile()).to.equal(true);
-        expect(fs.readFileSync(filename, "utf8")).to.equal("@import \"/assets/asset-0589f66713bc44029a1a720b9a0d850d.css\";\n\n");
+        expect(fs.readFileSync(filename, "utf8")).to.equal("@import \"/assets/asset-e89e45b0019f8d3feb5341de6b815cdb.css\";\n\n");
 
         process.env.NODE_ENV = env;
         rmrf(dir, done);
